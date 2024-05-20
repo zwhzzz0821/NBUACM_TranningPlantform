c211edf36c69f0cdec59f68b9f9a48fe99977d4d
hstxmgzY <244046581@qq.com>
2024-05-20 08:31:01 +0800

    feat:首页添加honor

---
+++ docs/.vitepress/theme/components/Activities.vue
@@ -0,0 +1,13 @@
+<template>
+  https://codepen.io/JavaScriptJunkie/pen/ZMMRRQ
+</template>
+
+<script>
+export default {
+  name: "Activities"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,28 @@
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
+import DefaultTheme from 'vitepress/theme'
+import './style.css'
+import Honor from "./components/Honor.vue";
+import Activities from "./components/Activities.vue";
+import 'element-plus/dist/index.css';
+import ElementPlus from 'element-plus';
+
+import Avatar from './components/Avatar.vue'
+
+/** @type {import('vitepress').Theme} */
+export default {
+    extends: DefaultTheme,
+    Layout: () => {
+        return h(DefaultTheme.Layout, null, {
+            // https://vitepress.dev/guide/extending-default-theme#layout-slots
+            // 'nav-bar-content-after': () => h(Avatar),
+        })
+    },
+    enhanceApp({ app, router, siteData }) {
+        // app.honor('Honor', Honor)
+        app.use(ElementPlus);
+        app.component('Honor', Honor)
+        app.component('Activities', Activities)
+
+    }
+}
