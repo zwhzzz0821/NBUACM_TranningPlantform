e62fa9d60569e4e054568ccc2c470a9425876480
zwhzzz0821 <2831474076@qq.com>
2024-07-28 13:28:03 +0800

    加入了荣誉墙的分页功能，和做了部分bug的修正

--- web/src/components/admin/adminHornour.vue
+++ web/src/components/admin/adminHornour.vue
@@ -1,13 +1,13 @@
 <template>
   <el-container style="height: 100%">
-    <el-header style="height: 20%; line-height: 50px;">
+    <el-header style="height: 12%; line-height: 50px;">
       <h3 style="text-align: center;">荣誉墙管理</h3>
       <el-button type="primary" @click="exportExcel">导出Excel</el-button>
     </el-header>
     <el-divider></el-divider>
     <el-main>
       <el-table
-        :data="honors"
+        :data="pagedRows"
         id="honorsTable"
         style="width: 100%"
         stripe
@@ -18,13 +18,13 @@
             <template slot-scope="scope">
                 <div>
                     {{ scope.row.award }}
-											<img v-if="scope.row.award == '一等奖'" :src="goldimg" alt="">
-											<img v-if="scope.row.award == '二等奖'" :src="silverimg" alt="">
-											<img v-if="scope.row.award == '三等奖'" :src="bronzeimg" alt="">
+											<img v-if="scope.row.award == '金奖'" :src="goldimg" alt="">
+											<img v-if="scope.row.award == '银奖'" :src="silverimg" alt="">
+											<img v-if="scope.row.award == '铜奖'" :src="bronzeimg" alt="">
                 </div>
             </template>
         </el-table-column>
-        <el-table-column label="获得时间" prop="acquiredTime" align="center"></el-table-column>
+        <el-table-column label="获得时间" :formatter="timeStamp" prop="acquiredTime" align="center"></el-table-column>
         <el-table-column label="参赛队员1" prop="player1"></el-table-column>
         <el-table-column label="参赛队员2" prop="player2"></el-table-column>
         <el-table-column label="参赛队员3" prop="player3"></el-table-column>
@@ -37,6 +37,16 @@
         </el-table-column>
       </el-table>
       <el-button @click="addItem">新增荣誉</el-button>
+      <el-pagination
+          background
+          layout="prev, pager, next"
+          class="pagination-container"
+          :total="totalRows"
+          :current-page.sync="currentPage"
+          :page-size="pageSize"
+          @current-change="onPageChange"
+          style="margin: 0 auto; display: block;">
+      </el-pagination>
     </el-main>
     <el-dialog
       title="编辑荣誉"
@@ -45,7 +55,11 @@
     >
       <el-form :model="editForm" label-width="100px">
         <el-form-item label="奖项">
-            <el-input v-model="editForm.award"></el-input>
+          <el-select v-model="editForm.award" placeholder="请选择奖项">
+            <el-option label="金奖" value="金奖"></el-option>
+            <el-option label="银奖" value="银奖"></el-option>
+            <el-option label="铜奖" value="铜奖"></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="获得时间">
           <el-date-picker
@@ -86,7 +100,6 @@ import silverimg from "../../assets/silver.png"
 import bronzeimg from "../../assets/bronze.png"
 import FileSaver from 'file-saver'
 import * as XLSX from 'xlsx'
-import saveAs from 'file-saver';
 import request from "../../util/request";
 export default {
   data() {
@@ -94,12 +107,16 @@ export default {
       goldimg: goldimg,
 			silverimg: silverimg,
 			bronzeimg: bronzeimg,
+      currentPage: 1,
+      pagedRows: [],
+      pageSize: 10,
+      totalRows: 0,
       dialogVisible: false,
       uploadedFiles: [],
       editForm: {
         id: null,
         award: '',
-        acquiredTime: '',
+        acquiredTime: 0,
         player1: '',
         player2: '',
         player3: '',
@@ -108,7 +125,42 @@ export default {
       honors: []
     };
   },
+  computed: {
+      /**
+       * 根据当前页码和每页大小计算分页数据
+       */
+      currentPagedData() {
+        const start = (this.currentPage - 1) * this.pageSize;
+        const end = start + this.pageSize;
+        return this.honors.slice(start, end);
+      },
+  },
+  watch: {
+    // 监听原始数据变化，重新计算分页数据
+    honors(newRows) {
+      this.totalRows = newRows.length;
+      this.pagedRows = this.currentPagedData;
+    },
+  },
   methods: {
+    fetchHonors(pageNumber, pageSize) {
+      request.get("/honors/Get", {
+        params: {
+          pageNumber,
+          pageSize,
+        }
+      }).then(res => {
+        this.honors = res.honors; // 假设后端返回的是荣誉列表数据
+        this.totalRows = res.length; // 假设后端返回总记录数
+      });
+    },
+    onPageChange(page) {
+        this.currentPage = page;
+        this.pagedRows = this.currentPagedData;
+      },
+    updated() {
+      this.fetchHonors(this.currentPage, this.pageSize);
+    },
     addItem() {
       this.dialogVisible = true;
       this.editForm.id = null;
@@ -170,12 +222,25 @@ export default {
       // 返回一个新创建的Blob对象，其内容由参数中给定的数组串联组成。
       return wbout
     },
-		updated() {
-			request.get("/honors/Get").then(res =>{
-				this.honors = res.honors
-				console.log(res.honors)
-			})
-		},
+		// updated() {
+		// 	request.get("/honors/Get").then(res =>{
+		// 		this.honors = res.honors
+		// 		console.log(res.honors)
+		// 	})
+		// },
+    timeStamp(value) {
+
+      let date = new Date(value.acquiredTime); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+      let year = date.getFullYear();
+      let month = ("0" + (date.getMonth() + 1)).slice(-2);
+      let sdate = ("0" + date.getDate()).slice(-2);
+      let hour = ("0" + date.getHours()).slice(-2);
+      let minute = ("0" + date.getMinutes()).slice(-2);
+      let second = ("0" + date.getSeconds()).slice(-2);
+      let result = year + "." + month + "." + sdate;
+      // 返回
+      return result;
+    }
   },
 	created() {
 		this.updated()
